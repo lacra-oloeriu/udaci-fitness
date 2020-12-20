2fb75c6dd0991ed1dc874587d8e7016a273b0c96
Lacramioara <lacramioara.oloeriu@gmail.com>
2020-12-20 13:19:31 +0100

    pizza

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Ionicons name='ios-pizza' color='red' size= {100} />
     </View>
   );
 }
