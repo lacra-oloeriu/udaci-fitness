da0cdade28488a6391344f38824b2658d3cc916c
Lacramioara <lacramioara.oloeriu@gmail.com>
2020-12-25 10:55:58 +0100

    created AddEntry

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {Ionicons} from '@expo/vector-icons';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import AddEntry from "./components/AddEntry";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Ionicons name='ios-pizza' color='red' size= {100} />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <AddEntry/>
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
