2ad8500a7eed1f9bf39377c51569415827a7c911
Lacramioara <lacramioara.oloeriu@gmail.com>
2020-12-26 11:08:22 +0100

    added getmetainfo

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import { getMetricMetaInfo } from "../utils/helpers";
 
 export default class AddEntry extends Component {
   render() {
     return (
       <View>
-        <Text>Add entry</Text>
+        {getMetricMetaInfo('bike').getIcon()}
       </View>
     );
   }
